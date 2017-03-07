1fd6afeb1049b99edbfd378a454b5b53c80df7c3
youriharmsen <y.j.g.harmsen@gmail.com>
2017-03-07 15:20:22 +0100

    printing ingredients

--- carbonara.rb
+++ carbonara.rb
@@ -12,10 +12,18 @@ NUM_GARLICCLOVES = 2
 NUM_EGGS = 3
 
 ingredients = [
-  {name: "garlic cloves", quantity: NUM_GARLICCLOVES}
-  {name: "eggs", quantity: NUM_EGGS}
-  {name: "bacon", quantity: "250 grams"}
-  {name: "spaghetti", quantity: "200 grams"}
-  {name: "parmesan", quantity: "some"}
-
+  {name: "garlic cloves", quantity: NUM_GARLICCLOVES},
+  {name: "eggs", quantity: NUM_EGGS},
+  {name: "bacon", quantity: "250 grams"},
+  {name: "spaghetti", quantity: "200 grams"},
+  {name: "Parmesan", quantity: "Some"},
 ]
+
+puts "*  Let's cook a nice Spaghetti Carbonara!  *"
+print_divider
+
+puts "You need the following ingredients:"
+
+ingredients.each do |ingredient|
+  puts "*  #{ingredient[:quantity]} #{ingredient[:name]} "
+end
